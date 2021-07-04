84bee2cdeb9df1bf2cf094a2ebd38ab947e820b6
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-07-04 12:13:16 +0200

    Move headers to include/ and .c files to src/. Format proj. with clang-format

--- include/libBigWig/bwCommon.h
+++ include/libBigWig/bwCommon.h
@@ -1,13 +1,17 @@
 /*! \file bwCommon.h
  *
- * You have no reason to use these functions. They may change without warning because there's no reason for them to be used outside of libBigWig's internals.
+ * You have no reason to use these functions. They may change without warning because there's no
+ * reason for them to be used outside of libBigWig's internals.
  *
- * These are structures and functions from a variety of files that are used across files internally but don't need to be see by libBigWig users.
+ * These are structures and functions from a variety of files that are used across files internally
+ * but don't need to be see by libBigWig users.
  */
 
 /*!
  * @brief Like fsetpos, but for local or remote bigWig files.
- * This will set the file position indicator to the specified point. For local files this literally is `fsetpos`, while for remote files it fills a memory buffer with data starting at the desired position.
+ * This will set the file position indicator to the specified point. For local files this literally
+ * is `fsetpos`, while for remote files it fills a memory buffer with data starting at the desired
+ * position.
  * @param fp A valid opened bigWigFile_t.
  * @param pos The position within the file to seek to.
  * @return 0 on success and -1 on error.
@@ -22,7 +26,8 @@ int bwSetPos(bigWigFile_t *fp, size_t pos);
  * @param nmemb The number of members to copy.
  * @param fp The bigWigFile_t * from which to copy the data.
  * @see bwSetPos
- * @return For nmemb==1, the size of the copied data. For nmemb>1, the number of members fully copied (this is equivalent to `fread`).
+ * @return For nmemb==1, the size of the copied data. For nmemb>1, the number of members fully
+ * copied (this is equivalent to `fread`).
  */
 size_t bwRead(void *data, size_t sz, size_t nmemb, bigWigFile_t *fp);
 
@@ -36,7 +41,8 @@ long bwTell(bigWigFile_t *fp);
 
 /*!
  * @brief Reads a data index (either full data or a zoom level) from a bigWig file.
- * There is little reason for end users to use this function. This must be freed with `bwDestroyIndex`
+ * There is little reason for end users to use this function. This must be freed with
+ * `bwDestroyIndex`
  * @param fp A valid bigWigFile_t pointer
  * @param offset The file offset where the index begins
  * @return A bwRTree_t pointer or NULL on error.
@@ -51,19 +57,22 @@ void bwDestroyIndexNode(bwRTreeNode_t *node);
 
 /*!
  * @brief Frees space allocated by `bwReadIndex`
- * There is generally little reason to use this, since end users should typically not need to run `bwReadIndex` themselves.
+ * There is generally little reason to use this, since end users should typically not need to run
+ * `bwReadIndex` themselves.
  * @param idx A bwRTree_t pointer allocated by `bwReadIndex`.
  */
 void bwDestroyIndex(bwRTree_t *idx);
 
 /// @cond SKIP
-bwOverlapBlock_t *walkRTreeNodes(bigWigFile_t *bw, bwRTreeNode_t *root, uint32_t tid, uint32_t start, uint32_t end);
+bwOverlapBlock_t *walkRTreeNodes(bigWigFile_t *bw, bwRTreeNode_t *root, uint32_t tid,
+                                 uint32_t start, uint32_t end);
 void destroyBWOverlapBlock(bwOverlapBlock_t *b);
 /// @endcond
 
 /*!
  * @brief Finishes what's needed to write a bigWigFile
- * Flushes the buffer, converts the index linked list to a tree, writes that to disk, handles zoom level stuff, writes magic at the end
+ * Flushes the buffer, converts the index linked list to a tree, writes that to disk, handles zoom
+ * level stuff, writes magic at the end
  * @param fp A valid bigWigFile_t pointer
  * @return 0 on success
  */
