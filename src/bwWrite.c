bda7cbfb803430ff5c7f1b0d2e7b74a63fb29a97
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-10-22 18:39:55 +0200

    Fix memory leak

--- src/bwWrite.c
+++ src/bwWrite.c
@@ -198,7 +198,9 @@ int bwWriteHdr(bigWigFile_t *bw) {
   uint32_t magic = BIGWIG_MAGIC;
   uint16_t two = 4;
   FILE *fp;
-  void *p = calloc(58, sizeof(uint8_t));  // 58 bytes of nothing
+  // void *p = calloc(58, sizeof(uint8_t));  // 58 bytes of nothing
+  uint8_t pbuff[58] = {0};
+  void *p = (void *)&pbuff;
   if (!bw->isWrite) return 1;
 
   // The header itself, largely just reserving space...
@@ -233,7 +235,6 @@ int bwWriteHdr(bigWigFile_t *bw) {
   // Save space for the number of blocks
   if (fwrite(p, sizeof(uint8_t), 8, fp) != 8) return 13;
 
-  free(p);
   return 0;
 }
 
